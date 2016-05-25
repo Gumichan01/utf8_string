afdfb198b726eff860902104b723bcabf5e06f9f
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-05-25 21:21:50 +0200

    Fixed clang warning

--- src/utf8_types.hpp
+++ src/utf8_types.hpp
@@ -1,7 +1,7 @@
 #ifndef UTF8_TYPES_HPP_INCLUDED
 #define UTF8_TYPES_HPP_INCLUDED
 
-typedef unsigned char byte_t;
+typedef char byte_t;
 typedef size_t utf8_len_t;
 
 #endif // UTF8_TYPES_HPP_INCLUDED
