42900defa84cf215486fadb7d7c83596e53dbe98
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-05-29 14:10:11 +0200

    Refactored utf8_substr and utf8_find

--- src/utf8_string.cpp
+++ src/utf8_string.cpp
@@ -318,16 +318,10 @@ UTF8string UTF8string::utf8_substr(size_t pos,size_t len) const
     const size_t n = (len == npos || (pos + len) > utf8length) ?
                         (utf8length - pos) : (pos + len - pos);
     // First position in bytes
-    size_t beginsz = utf8_bpos_at(pos);
-    const size_t u8size = utf8_size();
-
-    /*for(size_t i = 0; beginsz < u8size && i < pos; i++)
-    {
-        beginsz += utf8_codepoint_len_(beginsz);
-    }*/
-
     std::string s;
     size_t u8count = 0;
+    const size_t u8size = utf8_size();
+    const size_t beginsz = utf8_bpos_at(pos);
 
     for(size_t j = beginsz; j < u8size && u8count < n;)
     {
@@ -352,11 +346,6 @@ size_t UTF8string::utf8_find(const UTF8string& str, size_t pos) const
     // Go to the position
     size_t beginsz = utf8_bpos_at(pos);
     const size_t u8size = utf8_size();
-
-    /*for(size_t i = 0; beginsz < u8size && i < pos; i++)
-    {
-        beginsz += utf8_codepoint_len_(beginsz);
-    }*/
 
     // Look for the string
     while(beginsz < u8size)
