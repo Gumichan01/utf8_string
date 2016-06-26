8742c3e63a5dd5dcc7e69bb94dc9f83265d745d6
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-06-26 16:18:00 +0200

    Documented npos and modified its value

--- src/utf8_string.hpp
+++ src/utf8_string.hpp
@@ -45,7 +45,23 @@ class UTF8string
 
 public:
 
-    const static size_t npos = std::string::npos;
+    /**
+    *   @var npos
+    *
+    *   *npos* is a static member constant value with the greatest
+    *   possible value for an element of type *size_t*.
+    *
+    *   This value, when used as the value for a len parameter in some
+    *   UTF-8 string's member functions(utf8_substr()),
+    *   means "until the end of the utf-8 string".
+    *
+    *   As a return value in utf8_find(), it is used to indicate no matches.
+    *   This constant is defined with a value of -1,
+    *   which because *size_t* is an unsigned integral type,
+    *   it is the largest possible representable value for this type.
+    *
+    */
+    const static size_t npos = -1;
 
     /**
     *   @fn UTF8string()
