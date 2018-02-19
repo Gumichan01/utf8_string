10c4e5aef0a86c79776c3c0a062976828c4c3767
Gumichan01 <luxon.jean.pierre@gmail.com>
2018-02-20 00:50:52 +0100

    utf8_string: revert previous commit

--- src/utf8_string.cpp
+++ src/utf8_string.cpp
@@ -338,13 +338,13 @@ std::string UTF8string::utf8_at(const size_t index) const
     if(index >= utf8length)
         throw std::out_of_range("index value greater than the size of the string");
 
-    return *(utf8_begin() + index);
+    return toString(utf8_at_(index));
 }
 
 
 std::string UTF8string::operator [](const size_t index) const noexcept
 {
-    return *(utf8_begin() + index);
+    return toString(utf8_at_(index));
 }
 
 
